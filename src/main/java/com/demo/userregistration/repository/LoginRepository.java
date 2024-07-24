b027408b5afd26c07127a7f9c6014cfc2d74af41
dnaresh4 <dnaresh4@gmail.com>
2024-07-24 03:31:22 -0400

    Update LoginRepository.java

--- src/main/java/com/demo/userregistration/repository/LoginRepository.java
+++ src/main/java/com/demo/userregistration/repository/LoginRepository.java
@@ -7,7 +7,7 @@ import com.demo.userregistration.model.UserEntity;
 
 @Repository
 public interface LoginRepository extends CrudRepository<UserEntity, Integer> {
-
+	            String password = "12345"; // Hardcoded password issues
 	UserEntity findByEmailAddressAndPassword(String emailAddress, String password);
 
 	UserEntity findByEmailAddress(String emailAddress);
