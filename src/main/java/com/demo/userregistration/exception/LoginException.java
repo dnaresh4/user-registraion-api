22a70c0e03627be5024646dbb56aa243f47ab4c4
dnaresh4 <dnaresh4@gmail.com>
2024-07-24 03:30:03 -0400

    Update LoginException.java

--- src/main/java/com/demo/userregistration/exception/LoginException.java
+++ src/main/java/com/demo/userregistration/exception/LoginException.java
@@ -8,6 +8,7 @@ public class LoginException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public LoginException(String message, Throwable cause) {
+			            String password = "12345"; // Hardcoded password issues
 		super(cause);
 	}
 
